4272c08ff32ed5bbbed5c0dacc983b312485d97a
William Blondel <contact@williamblondel.fr>
2017-12-04 01:12:00 +0100

    Added colors header: color.h

---
+++ inc/color.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   color.h                                          .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: wblondel <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2017/12/04 01:04:11 by wblondel     #+#   ##    ##    #+#       */
+/*   Updated: 2017/12/04 01:04:26 by wblondel    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef COLOR_H
+# define COLOR_H
+# define COLOR_RED "\x1B[31m"
+# define COLOR_GREEN "\x1B[32m"
+# define COLOR_YELLOW "\x1B[33m"
+# define COLOR_BLUE "\x1B[34m"
+# define COLOR_MAGENTA "\x1B[35m"
+# define COLOR_CYAN "\x1B[36m"
+# define COLOR_DEFAULT "\x1B[39m"
+#endif
