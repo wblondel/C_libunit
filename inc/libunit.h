7c592c556eccf978afd05f9d7eee1dac8dc0cfd0
William Blondel <contact@williamblondel.fr>
2017-12-04 01:15:13 +0100

    libunit.h: added global variables for test results

--- inc/libunit.h
+++ inc/libunit.h
@@ -6,7 +6,7 @@
 /*   By: wblondel <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/12/04 01:05:46 by wblondel     #+#   ##    ##    #+#       */
-/*   Updated: 2017/12/04 01:11:33 by wblondel    ###    #+. /#+    ###.fr     */
+/*   Updated: 2017/12/04 01:14:49 by wblondel    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -14,9 +14,15 @@
 #ifndef LIBUNIT_H
 # define LIBUNIT_H
 
+/* Includes */
 # include <stdlib.h>
 # include <unistd.h>
 # include <sys/wait.h>
 # include <signal.h>
+
+/* Test result global variables */
+# define TEST_SUCCESS 0
+# define TEST_ERROR -1
+# define TEST_TIMEOUT 10
 
 #endif
